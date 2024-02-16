834e84aa3df510f2ec9f24a4f5fcf224fe264701
nazanin <dev.nzniing@gmail.com>
2024-02-16 23:12:06 +0330

    Create Offer Component Design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import Notification from "@/components/Notification";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Home from "./page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Notification />
+      <Navbar />
+      {children}
+      <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/components/CountDown.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import Countdown from 'react-countdown'
+
+const endingDate = new Date("2024-07-25")
+
+function CountDown() {
+  return (
+    <div>
+        <Countdown className='font-bold text-5xl text-yellow-300' date={endingDate}/>
+    </div>
+  )
+  
+}
+
+export default CountDown
